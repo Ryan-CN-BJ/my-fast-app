8fc59529dfed184574b555f7f47c8d2f3301cec2
Ryan-CN-BJ <maorunjie2025@qq.com>
2026-07-30 23:10:02 +0800

    feat:练习sqlalchemy的create_async_engine,async_sessionmaker,get_db等模块；安装alembic相关依赖,初始化alembic

---
+++ apps/userservice/src/core/config/dbconfig.py
@@ -0,0 +1,13 @@
+from core.config.commonConfig import CommonSettings
+
+
+class DbSetting(CommonSettings):
+    user: str = ""
+    password: str = ""
+    host: str = ""
+    port: str = ""
+    name: str = ""
+    model_config = {"env_prefix": "DB_"}
+
+
+dbSetting = DbSetting()
