93ba69d9bc7c5b717e7c21082a5e198d5622b7ee
Ryan-CN-BJ <maorunjie2025@qq.com>
2026-08-04 20:38:32 +0800

    feat:优化pydantic sku product schema

--- apps/userservice/src/schema/sku.py
+++ apps/userservice/src/schema/sku.py
@@ -1,11 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from decimal import Decimal
+
+from typing import Annotated
+from model.sku import MAX_SKU_CODE_LENGTH, MAX_PRICE_DIGITS, PRICE_DECIMAL_PLACES
 
 
 class CreateSku(BaseModel):
     product_id: int
-    sku_code: str
-    price: Decimal
+    sku_code: Annotated[str, Field(max_length=MAX_SKU_CODE_LENGTH)]
+    price: Annotated[
+        Decimal, Field(max_digits=MAX_PRICE_DIGITS, decimal_places=PRICE_DECIMAL_PLACES)
+    ]
     stock: int
     attrs: dict
     image_url: str
