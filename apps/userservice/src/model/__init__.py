4c01cf278110c579e99d2cba46e8375da4ee64aa
Ryan-CN-BJ <maorunjie2025@qq.com>
2026-08-05 15:17:22 +0800

    feat:创建category model

--- apps/userservice/src/model/__init__.py
+++ apps/userservice/src/model/__init__.py
@@ -1,3 +1,4 @@
 from model import user
 from model import product
 from model import sku
+from model import category
---
+++ apps/userservice/src/model/category.py
@@ -0,0 +1,15 @@
+from model.base import Base
+
+from typing import Annotated
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+
+MAX_NAME_LENGTH = 50
+MAX_DESCRIPTION_LENGTH = 200
+
+
+class Category(Base):
+    name: Mapped[Annotated[str, mapped_column(String(MAX_NAME_LENGTH), nullable=False)]]
+    description: Mapped[
+        Annotated[str, mapped_column(String(MAX_DESCRIPTION_LENGTH), nullable=True)]
+    ]
