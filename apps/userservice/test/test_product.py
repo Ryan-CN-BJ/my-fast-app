d8b1ecd1b29ea7dbf0d4811986c995a73ee38c60
Ryan-CN-BJ <maorunjie2025@qq.com>
2026-08-17 17:28:30 +0800

    feat:pytest loop scoped设置

--- apps/userservice/test/test_product.py
+++ apps/userservice/test/test_product.py
@@ -12,3 +12,17 @@ class TestProduct:
         assert res.status_code == 200
         assert data["brand"] == "123"
         assert data["id"] is not None
+
+    async def test_create_get_product(self, async_client: AsyncClient):
+        res = await async_client.post(
+            "/product/add",
+            json={"brand": "mrj", "name": "测试pytest", "description": "123"},
+        )
+        id = res.json()["data"]["id"]
+
+        response = await async_client.get(
+            "/product/query/productwithskus", params={"id": id}
+        )
+        product = response.json()["data"]
+        assert product["brand"] == "mrj"
+        assert product["name"] == "测试pytest"
