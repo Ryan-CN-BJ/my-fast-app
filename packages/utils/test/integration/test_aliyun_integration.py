46b6c50f9333ad2b8ca330dc7bd326af4143359c
Ryan-CN-BJ <maorunjie2025@qq.com>
2026-08-25 15:27:17 +0800

    feat:阿里云oss上传-集成测试

--- packages/utils/test/integration/test_aliyun_integration.py
+++ packages/utils/test/integration/test_aliyun_integration.py
@@ -1,23 +1,33 @@
 import httpx
+from dotenv import load_dotenv
+from pathlib import Path
+import base64
+
+ENV_FILE = Path(__file__).parent.parent.parent.parent.parent / ".env.test"
+load_dotenv(ENV_FILE)
+
 from utils.aliyun import generate_upload_params
 
 
 class TestAliyunUpload:
     async def test_upload(self):
         cred = await generate_upload_params("1.png")
+
+        IMAGE = Path(__file__).parent / "tiny.png"
+        image_bytes = IMAGE.read_bytes()
         res = httpx.post(
-            cred.host,
+            cred["host"],
             data={
                 "success_action_status": "200",
                 "policy": cred["policy"],
-                "x-oss-signature-version": cred["x-oss-signature-version"],
+                "x-oss-signature-version": cred["x_oss_signature_version"],
                 "x-oss-signature": cred["signature"],
-                "x-oss-credential": cred["x-oss-credential"],
-                "x-oss-date": cred["x-oss-date"],
+                "x-oss-credential": cred["x_oss_credential"],
+                "x-oss-date": cred["x_oss_date"],
                 "key": cred["key"],
-                "x-oss-security-token": cred["x-oss-security-token"],
-                "content-type": cred["content-type"],
+                "x-oss-security-token": cred["security_token"],
+                "content-type": cred["content_type"],
             },
-            files={"file": ("1.png", "123", cred.content_type)},
+            files={"file": ("1.png", image_bytes, cred["content_type"])},
         )
-        assert res.status == "200"
+        assert res.status_code == 200
